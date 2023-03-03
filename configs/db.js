43620dca1f5ced898a6665584d9d467551236bed
Sanjay V <74954064+Vsanjay021@users.noreply.github.com>
2023-03-04 00:34:24 +0530

    Update db.js

--- configs/db.js
+++ configs/db.js
@@ -1,4 +1,5 @@
 const mongoose=require("mongoose");
+mongoose.set("strictQuery", false);
 require("dotenv").config();
 const connection=mongoose.connect("mongodb+srv://sanjayv:sanjay@cluster0.9ycsbw8.mongodb.net/unit4project?retryWrites=true&w=majority");
 
